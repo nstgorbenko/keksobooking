4f0003f92d98faf608911b0319b3ad60082547d8
Nst Gorbenko <nstgorbenko@yandex.ru>
2019-06-12 18:17:21 +0300

    Правки. Названия переменных, порядок кода

--- js/main.js
+++ js/main.js
@@ -5,54 +5,50 @@ var MAP_LEFT = 0;
 var MAP_RIGHT = 980;
 var MAP_TOP = 130;
 var MAP_BOTTOM = 630;
+var PINS_NUMBER = 8;
 
-var createArray = function () {
-  var array = [];
-  var typesList = ['palace', 'flat', 'house', 'bungalo'];
-  var randomNumber = function (min, max) {
-    var random = min + Math.random() * (max + 1 - min);
-    random = Math.floor(random);
-    return random;
-  };
+var getRandomNumber = function (min, max) {
+  return Math.floor(min + Math.random() * (max + 1 - min));
+};
 
-  for (var i = 1; i <= 8; i++) {
-    var newObject = {};
-    newObject.author = {'avatar': 'img/avatars/user0' + i + '.png'};
-    newObject.offer = {'type': typesList[Math.floor(Math.random() * typesList.length)]};
-    newObject.location = {};
-    newObject.location.x = randomNumber(MAP_LEFT, MAP_RIGHT);
-    newObject.location.y = randomNumber(MAP_TOP, MAP_BOTTOM);
-    array.push(newObject);
+var createAds = function () {
+  var adsList = [];
+
+  for (var i = 1; i <= PINS_NUMBER; i++) {
+    var newAd = {};
+    newAd.author = {'avatar': 'img/avatars/user0' + i + '.png'};
+    newAd.offer = {'type': typesList[Math.floor(Math.random() * typesList.length)]};
+    newAd.location = {};
+    newAd.location.x = getRandomNumber(MAP_LEFT, MAP_RIGHT);
+    newAd.location.y = getRandomNumber(MAP_TOP, MAP_BOTTOM);
+    adsList.push(newAd);
   }
-  return array;
+  return adsList;
 };
 
-var nearbyAds = createArray();
-
-var map = document.querySelector('.map');
-map.classList.remove('map--faded');
-
-var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
-
-var createMapMark = function (mapMarkObject) {
-  var mapMark = pinTemplate.cloneNode(true);
-  mapMark.style.left = mapMarkObject.location.x - PIN_WIDTH / 2 + 'px';
-  mapMark.style.top = mapMarkObject.location.y - PIN_HEIGHT + 'px';
-  mapMark.querySelector('img').src = mapMarkObject.author.avatar;
-  mapMark.querySelector('img').alt = '';
-  return mapMark;
+var createPin = function (pinData) {
+  var pin = pinTemplate.cloneNode(true);
+  pin.style.left = pinData.location.x - PIN_WIDTH / 2 + 'px';
+  pin.style.top = pinData.location.y - PIN_HEIGHT + 'px';
+  pin.querySelector('img').src = pinData.author.avatar;
+  pin.querySelector('img').alt = '';
+  return pin;
 };
 
-var createMapMarksElement = function (mapMarksArray) {
+var createPinsList = function (ads) {
   var fragment = document.createDocumentFragment();
-  for (var i = 0; i < mapMarksArray.length; i++) {
-    var newMapMark = createMapMark(mapMarksArray[i]);
-    fragment.appendChild(newMapMark);
-  }
+  ads.forEach(function (newPin) {
+    fragment.appendChild(createPin(newPin));
+  });
   return fragment;
 };
 
-var mapMarksElement = createMapMarksElement(nearbyAds);
-
+var typesList = ['palace', 'flat', 'house', 'bungalo'];
+var map = document.querySelector('.map');
+var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 var mapPins = document.querySelector('.map__pins');
-mapPins.appendChild(mapMarksElement);
+
+map.classList.remove('map--faded');
+var nearbyAds = createAds();
+var pinsList = createPinsList(nearbyAds);
+mapPins.appendChild(pinsList);

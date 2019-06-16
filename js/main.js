bc1bcb4b0ab1e1636962a815faef25ded0c1b024
Nst Gorbenko <nstgorbenko@yandex.ru>
2019-06-16 19:08:51 +0300

    Правки. Форматирование

--- js/main.js
+++ js/main.js
@@ -6,20 +6,25 @@ var MAP_RIGHT = 980;
 var MAP_TOP = 130;
 var MAP_BOTTOM = 630;
 var PINS_NUMBER = 8;
+var TYPES_LIST = ['palace', 'flat', 'house', 'bungalo'];
 
 var getRandomNumber = function (min, max) {
   return Math.floor(min + Math.random() * (max + 1 - min));
 };
 
 var createAd = function (index) {
-  var newAd = {};
-
-  newAd.author = {'avatar': 'img/avatars/user0' + index + '.png'};
-  newAd.offer = {'type': typesList[Math.floor(Math.random() * typesList.length)]};
-  newAd.location = {};
-  newAd.location.x = getRandomNumber(MAP_LEFT, MAP_RIGHT);
-  newAd.location.y = getRandomNumber(MAP_TOP, MAP_BOTTOM);
-  return newAd;
+  return {
+    author: {
+      avatar: 'img/avatars/user0' + index + '.png'
+    },
+    offer: {
+      type: TYPES_LIST[Math.floor(Math.random() * TYPES_LIST.length)]
+    },
+    location: {
+      x: getRandomNumber(MAP_LEFT, MAP_RIGHT),
+      y: getRandomNumber(MAP_TOP, MAP_BOTTOM)
+    }
+  };
 };
 
 var createAds = function () {
@@ -28,6 +33,7 @@ var createAds = function () {
   for (var i = 1; i <= PINS_NUMBER; i++) {
     adsList.push(createAd(i));
   }
+
   return adsList;
 };
 
@@ -38,6 +44,7 @@ var createPin = function (pinData) {
   pin.style.top = pinData.location.y - PIN_HEIGHT + 'px';
   pin.querySelector('img').src = pinData.author.avatar;
   pin.querySelector('img').alt = '';
+
   return pin;
 };
 
@@ -47,15 +54,15 @@ var createPinsList = function (ads) {
   ads.forEach(function (newPin) {
     fragment.appendChild(createPin(newPin));
   });
+
   return fragment;
 };
 
-var typesList = ['palace', 'flat', 'house', 'bungalo'];
 var map = document.querySelector('.map');
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 var mapPins = document.querySelector('.map__pins');
 
-map.classList.remove('map--faded');
 var nearbyAds = createAds();
 var pinsList = createPinsList(nearbyAds);
 mapPins.appendChild(pinsList);
+map.classList.remove('map--faded');
